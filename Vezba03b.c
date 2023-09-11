a3fc49e3f429a24ae8dfe81f83a5fcd2d4b9d323
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2023-09-11 15:40:37 +0200

    Add project files.

---
+++ Vezba03b.c
@@ -0,0 +1,29 @@
+// Vezba03b.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Sortiranje - Selection sort - optimalna verzija	
+#include <stdio.h>
+int main()
+{
+	// nesortirani niz
+	int niz[10] = { 5, 2, 7, 4, 1, 9, 3, 8, 6, 0 };
+	int i, j, temp, jmin; // i-levi element (prvi u nesortiranom delu), j-desni element
+	int n = 10; // broj elemenata niza
+	for (i = 0; i < n-1; i++)// do pretposlednjeg elementa
+	{
+		jmin = i + 1; // jmin indeks najmanjeg u nesortiranom delu
+		for ( j = i+1; j < n; j++) //desno od i-tog
+		{
+			if (niz[jmin] > niz[j]) // ako je j-ti manji od jmin
+			{
+				jmin = j;
+			}
+		}
+		temp = niz[i]; // zameni i-ti sa jmin
+		niz[i] = niz[jmin];
+		niz[jmin] = temp;
+
+	}
+	// ispis sortiranog niza
+	for (i = 0; i < n; i++)
+		printf("%d ", niz[i]);
+}
+
